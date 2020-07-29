5e7bba69449fa99b2d28d9906320342195780c91
Antoine POPINEAU <antoine@popineau.eu>
2020-07-29 11:48:46 +0200

    Initial commit.

---
+++ src/js/fastmail.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           Modern Fastmail
+// @namespace      https://github.com/apognu/modern-fastmail
+// @version        1.0.0
+// @description    Give a fresh look to Fastmail web interface
+// @author         Antoine POPINEAU <antoine.popineau@appscho.com>
+// @match          https://*.fastmail.com/*
+// ==/UserScript==
